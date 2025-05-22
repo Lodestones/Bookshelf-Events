a4d21d228f4fcfbdebc5b07bbe1f30d1f04e9cb6
John Aquino <41652412+apollyon600@users.noreply.github.com>
2025-05-22 01:02:45 -0700

    fix(events): reverted to old chat event

--- src/main/java/gg/lode/bookshelf/event/PlayerChatEvent.java
+++ src/main/java/gg/lode/bookshelf/event/PlayerChatEvent.java
@@ -2,128 +2,162 @@ package gg.lode.bookshelf.event;
 
 import gg.lode.bookshelfapi.api.event.BaseEvent;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.bukkit.event.Cancellable;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-/**
- * Event called when a player sends a chat message.
- * This event allows for customization of the chat message format and content.
- *
- * @author John Aquino
- */
-public class PlayerChatEvent extends BaseEvent {
+public class PlayerChatEvent extends BaseEvent implements Cancellable {
+
     private final Player player;
-    private Component message;
     private Component prefix;
     private Component suffix;
-    private String messageColor;
-    private String playerColor;
+    private Component message;
+    private boolean isCancelled;
     private String permission;
+    private String playerColor;
+    private String messageColor;
     private List<UUID> viewers;
-    private boolean cancelled;
-    private boolean modified;
+    private boolean isModified;
 
-    public PlayerChatEvent(@NotNull Player player, @NotNull Component message) {
-        super(true);
+    public PlayerChatEvent(Player player, Component prefix, Component suffix, Component message, String permission) {
         this.player = player;
-        this.message = message;
-        this.viewers = new ArrayList<>();
-        this.messageColor = "#FFFFFF";
-        this.playerColor = "#FFFFFF";
-        this.modified = false;
-    }
-
-    @NotNull
-    public Player getPlayer() {
-        return player;
-    }
-
-    @NotNull
-    public Component message() {
-        return message;
-    }
-
-    public void message(@NotNull Component message) {
-        this.message = message;
-        this.modified = true;
-    }
-
-    @Nullable
-    public Component prefix() {
-        return prefix;
-    }
-
-    public void setModified(boolean modified) {
-        this.modified = modified;
-    }
-
-    @NotNull
-    public List<UUID> getViewers() {
-        return viewers;
-    }
-
-    public void prefix(@Nullable Component prefix) {
         this.prefix = prefix;
-        this.modified = true;
-    }
-
-    @Nullable
-    public Component suffix() {
-        return suffix;
-    }
-
-    public void suffix(@Nullable Component suffix) {
         this.suffix = suffix;
-        this.modified = true;
+        this.isModified = false;
+        this.message = message;
+        this.permission = permission;
+        this.playerColor = NamedTextColor.GRAY.asHexString();
+        this.messageColor = NamedTextColor.GRAY.asHexString();
+        this.viewers = new ArrayList<>();
     }
 
-    @NotNull
-    public String messageColor() {
-        return messageColor;
+    public PlayerChatEvent(Player player, Component prefix, Component suffix, Component message) {
+        this.player = player;
+        this.prefix = prefix;
+        this.suffix = suffix;
+        this.message = message;
+        this.permission = null;
+        this.isModified = false;
+        this.playerColor = NamedTextColor.GRAY.asHexString();
+        this.messageColor = NamedTextColor.GRAY.asHexString();
+        this.viewers = new ArrayList<>();
     }
 
-    public void messageColor(@NotNull String messageColor) {
+    public PlayerChatEvent(Player player, Component prefix, Component message) {
+        this.player = player;
+        this.prefix = prefix;
+        this.suffix = Component.empty();
+        this.message = message;
+        this.permission = null;
+        this.playerColor = NamedTextColor.GRAY.asHexString();
+        this.messageColor = NamedTextColor.GRAY.asHexString();
+        this.viewers = new ArrayList<>();
+        this.isModified = false;
+    }
+
+    public PlayerChatEvent(Player player, Component message) {
+        this.player = player;
+        this.prefix = Component.empty();
+        this.suffix = Component.empty();
+        this.message = message;
+        this.permission = null;
+        this.playerColor = NamedTextColor.GRAY.asHexString();
+        this.messageColor = NamedTextColor.GRAY.asHexString();
+        this.viewers = new ArrayList<>();
+        this.isModified = false;
+    }
+
+    public void messageColor(String messageColor) {
         this.messageColor = messageColor;
-        this.modified = true;
+        this.isModified = true;
     }
 
-    @NotNull
-    public String playerColor() {
-        return playerColor;
+    public String messageColor() {
+        return this.messageColor;
     }
 
-    public void playerColor(@NotNull String playerColor) {
+    public void playerColor(String playerColor) {
         this.playerColor = playerColor;
-        this.modified = true;
+        this.isModified = true;
     }
 
-    @Nullable
+    public String playerColor() {
+        return this.playerColor;
+    }
+
     public String getPermission() {
         return permission;
     }
 
-    public void setPermission(@Nullable String permission) {
+    public void setPermission(String permission) {
         this.permission = permission;
+        this.isModified = true;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Component prefix() {
+        return prefix;
+    }
+
+    public Component suffix() {
+        return suffix;
+    }
+
+    public Component message() {
+        return message;
+    }
+
+    public void prefix(Component component) {
+        this.prefix = component;
+        this.isModified = true;
+    }
+
+    public void suffix(Component component) {
+        this.suffix = component;
+        this.isModified = true;
+    }
+
+    public void message(Component component) {
+        this.message = component;
+        this.isModified = true;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+        this.isCancelled = b;
+    }
+
+    /**
+     * Has this event been touched by other plugins?
+     *
+     * @return true if touched by other plugins.
+     */
+    public boolean isModified() {
+        return isModified;
+    }
+
+    public void setModified(boolean modified) {
+        isModified = modified;
+    }
+
+    public List<UUID> getViewers() {
+        return viewers;
     }
 
     public void setViewers(List<UUID> viewers) {
         this.viewers = viewers;
+        this.isModified = true;
     }
-
-    public boolean isCancelled() {
-        return cancelled;
-    }
-
-    public void setCancelled(boolean cancelled) {
-        this.cancelled = cancelled;
-    }
-
-    public boolean isModified() {
-        return modified;
-    }
-} 
+}
